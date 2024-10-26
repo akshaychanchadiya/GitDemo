9a1f161f337c357521ddadfd359b0bee3a99bc13
akshaychanchadiya <akshaychanchadiya@gmail.com>
2024-10-26 12:37:38 +0530

    American development team

--- src/main/java/GitPractice/GitPractice1.java
+++ src/main/java/GitPractice/GitPractice1.java
@@ -5,6 +5,12 @@ public class GitPractice1 {
 	public static void main(String[] args) {
 		
 		System.out.println("GIT");
+		
+		System.out.println("GIT");
+		
+		System.out.println("GIT");
+		
+		
 
 	}
 
